17b364d1001619c59181cc8635beb54bd18ad2e9
Purwarane02 <rishikesh@handsintechnology.com>
2024-02-08 17:14:51 +0530

    add: CouncilMinister

--- src/pages/Legislature/CouncilMinister.js
+++ src/pages/Legislature/CouncilMinister.js
@@ -35,8 +35,8 @@ const Minister = () => {
 
     return (
         <>
-            <div>
-                <section className="background ">
+            <div className="background ">
+                <section >
                     <div className="container-fluid justify-content-center section-top-space">
                         <ul className="breadcrumb">
                             <li>
@@ -148,6 +148,72 @@ const Minister = () => {
                                         }}
                                     >
                                         {Ministers[checkLang].list}
+                                        <div
+                                            className="underline"
+                                            style={{
+                                                position: "absolute",
+                                                bottom: "-5px",
+                                                left: "0",
+                                                width: "100%",
+                                                height: "5px",
+                                                background: "linear-gradient(to right, green, yellow)",
+                                                opacity: "1",
+                                            }}
+                                        />
+                                    </div>
+                                </div>
+                                <table
+                                    className="table-box-col2 table-min mt-4 mb-5"
+                                    style={{ border: "1px solid orange" }}
+                                >
+                                    <thead className="table-col2-min">
+                                        <tr className="table-head">
+                                            <th > {Ministers[checkLang].h1}</th>
+                                            <th> {Ministers[checkLang].h2}</th>
+                                            <th> {Ministers[checkLang].h3}</th>
+                                            <th> {Ministers[checkLang].h4}</th>
+                                            <th> {Ministers[checkLang].h5}</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                        <tr >
+                                            <td className="text-center p-2"> 1.</td>
+                                            <td className="w-30 p-2">
+                                                <Link
+
+                                                    className="d-flex justify-content-start align-items-center gap-3" style={{ textDecoration: "none" }}
+                                                >
+                                                    <img src={partiwise2}
+                                                        className="imageMem"
+                                                        alt="Member"
+                                                    />
+                                                    Mr.Narayan Tatu Rane
+                                                </Link>
+                                            </td>
+                                            <td className="  text-center p-2">
+                                                Industries, Sports,Environment and Selft-Employment
+                                            </td>
+                                            <td className="text-center p-3">
+                                                Minister
+                                            </td>
+                                            <td className="text-center p-3">
+                                                MAle
+                                            </td>
+                                        </tr>
+
+                                    </tbody>
+                                </table>
+
+                                <div className="about-head text-center ">
+                                    <div
+                                        id="about-text"
+                                        style={{
+                                            display: "inline-block",
+                                            position: "relative",
+                                        }}
+                                    >
+                                        {Ministers[checkLang].list2}
                                         <div
                                             className="underline"
                                             style={{
